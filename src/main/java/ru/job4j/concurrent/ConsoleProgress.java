0430a4a55c6a5cfddbe449a68e47e23b11e109fb
Salavat <Justdrumm@yandex.ru>
2025-01-31 20:15:32 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        int index = 0;
+        while (!Thread.interrupted()) {
+            System.out.print("\r Load: " + process[index % process.length]);
+            index++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
