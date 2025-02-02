e2d4064c59b91d5f68bb8ca36640e83d762ff27c
Salavat <Justdrumm@yandex.ru>
2025-02-02 16:18:27 +0300

    refactor: Wget class

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -3,37 +3,40 @@ package ru.job4j.thread;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
-    private static final String FILE_NAME = "downloaded_file";
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed) throws MalformedURLException {
         this.url = url;
         this.speed = speed;
+        this.fileName = new URL(url).getPath().substring(url.lastIndexOf('/') + 1);
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] buffer = new byte[1024];
             int bytesRead;
+            long startTime = System.currentTimeMillis();
+            long totalBytes = 0;
 
             while ((bytesRead = in.read(buffer, 0, buffer.length)) != -1) {
-                long startTime = System.currentTimeMillis();
-
                 fileOutputStream.write(buffer, 0, bytesRead);
-                System.out.printf("Downloaded: %d bytes%n", bytesRead);
+                totalBytes += bytesRead;
 
-                long timeTaken = System.currentTimeMillis() - startTime;
-                long expectedTime = (long) (1000.0 * bytesRead / speed);
-
-                long sleepTime = expectedTime - timeTaken;
-                if (sleepTime > 0) {
-                    Thread.sleep(sleepTime);
+                if (totalBytes >= speed) {
+                    long timeTaken = System.currentTimeMillis() - startTime;
+                    if (timeTaken < 1000) {
+                        Thread.sleep(1000 - timeTaken);
+                    }
+                    totalBytes = 0;
+                    startTime = System.currentTimeMillis();
                 }
             }
             System.out.println("Download complete!");
@@ -43,10 +46,9 @@ public class Wget implements Runnable {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, MalformedURLException {
         if (args.length < 2) {
-            System.out.println("Usage: java Wget <URL> <speed>");
-            return;
+            throw new IllegalArgumentException("Usage: java Wget <URL> <speed>");
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
