046ad1e0af4fbf3213fcf54045dfac2bdd269700
Salavat <Justdrumm@yandex.ru>
2025-02-04 20:05:30 +0300

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,48 @@
+package ru.job4j.cash;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class AccountStorage {
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        if (account == null || accounts.containsKey(account.id())) {
+            return false;
+        }
+        accounts.put(account.id(), account);
+        return true;
+    }
+
+    public synchronized boolean update(Account account) {
+        if (account == null || !accounts.containsKey(account.id())) {
+            return false;
+        }
+        accounts.put(account.id(), account);
+        return true;
+    }
+
+    public synchronized void delete(int id) {
+        accounts.remove(id);
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        if (fromId == toId || amount <= 0) {
+            return false;
+        }
+        Account accountToTransfer = accounts.get(toId);
+        Account accountFromTransfer = accounts.get(fromId);
+        if (accountToTransfer != null && !(accountFromTransfer.amount() < amount)) {
+            accounts.put(accountToTransfer.id(),
+                    new Account(accountToTransfer.id(), accountToTransfer.amount() + amount));
+            accounts.put(accountFromTransfer.id(),
+                    new Account(accountFromTransfer.id(), accountFromTransfer.amount() - amount));
+        }
+        return false;
+    }
+}
+
